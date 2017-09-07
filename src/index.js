58aae15fa961158dc3335406f923611ba04a5ef7
Zachary Iles <zack.iles@gmail.com>
2017-09-07 15:01:40 -0400

    build(npm): Updated depends

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-'use strict'
+import pkg from '../package.json';
 
-import pkg from '../package.json'
-
-console.log(pkg.description)
-console.log(`Version: ${pkg.version}`)
-console.log(`Author: ${pkg.author}`)
-console.log(`Licence: ${pkg.license}`)
-console.log(`Repository: ${pkg.homepage}`)
+console.log(pkg.description);
+console.log(`Version: ${pkg.version}`);
+console.log(`Author: ${pkg.author}`);
+console.log(`Licence: ${pkg.license}`);
+console.log(`Repository: ${pkg.homepage}`);
